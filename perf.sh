4e80b8400c421f33f573035eb445e3704f14fdb3
prow machine <prow@pingcap.com>
2020-08-14 17:23:45 +0800

    add clean and observe

--- perf.sh
+++ perf.sh
@@ -1,15 +1,47 @@
+# start: bash perf.sh start pd-server /home/lhy1024 funcgraph
+# start: bash perf.sh start pd-server /home/lhy1024 iostoop
+# close: bash perf.sh close pd-server /home/lhy1024 all
+# observe: bash perf.sh observe
+# clean: bashe perf.sh clean
+
+observe(){
+s=$(ps -aux | grep $1 | grep -v "grep" | awk '{print $2}')
+if [ -z "$s" ];then
+	echo "$1 is not running"
+else
+	echo "$1 is running"
+fi
+}
+
+observe_all(){
+observe iotop
+observe pidstat
+observe funcgraph
+observe iosnoop
+}
+
+if [ $# == 1 ] ; then
+	if [ $1 == "observe" ] ; then
+	observe_all
+	exit 0;
+	fi
+	if [ $1 == "clean" ] ; then
+	rm -rf /var/tmp/.ftrace-lock
+	echo "ftrace-lock is removed"
+	exit 0;
+	fi
+fi
+
 if [ $# != 4  ] ; then
 echo "USAGE: $0 option instance log_dir perf_func"
 exit 1;
 fi
 
-# start: bash perf.sh start pd-server /home/lhy1024 funcgraph
-# start: bash perf.sh start pd-server /home/lhy1024 iostoop
-# close: bash perf.sh close pd-server /home/lhy1024 all
-
 option=$1
 instance=$2
-pid=$(pidof $instance)
+pids=$(pidof $instance)
+arr=($pids)
+pid=${arr[0]}
 dir=$3
 perf=$4
 
@@ -29,14 +61,20 @@ if [[ "$perf" == "iosnoop" ]];then
 fi
 fi
 
-if [[ "$option" == "close" ]];then
-	echo "close iotop"
-	kill -9 $(ps -aux | grep bin/iotop | grep -v "grep" | awk '{print $2}')
-	echo "close pidstat"
-	kill -9 $(ps -aux | grep pidstat | grep -v "grep" | awk '{print $2}')
-	echo "close funcgraph"
-	kill -9 $(ps -aux | grep funcgraph | grep -v "grep" | awk '{print $2}')
-	echo "close iosnoop"
-	kill -9 $(ps -aux | grep iosnoop | grep -v "grep" | awk '{print $2}')
+close(){
+s=$(ps -aux | grep $1 | grep -v "grep" | awk '{print $2}')
+if [ -n "$s" ];then	
+echo "close $1"
+kill -9 $s
 fi
+}
+
+if [[ "$option" == "close" ]];then
+	close iotop
+	close pidstat
+	close funcgraph
+	close iosnoop
+fi
+
+observe_all
 
